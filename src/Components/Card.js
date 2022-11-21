8803321beba5d23259d3424c63b0d62ec4ad6134
Thinh Do <thinhdo@pop-os.localdomain>
2022-11-22 00:17:15 +0700

    Attach audio to button

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -3,8 +3,25 @@ import "./Card.css";
 import { AiFillSound } from "react-icons/ai";
 import { FaRegBookmark } from "react-icons/fa";
 import { IconContext } from "react-icons";
+import axios from "axios";
 
 function Card({ word, pos, BinhDinh, GiaLai, KonTum }) {
+  const playSound = (e) => {
+    axios
+      .post("https://www.ura.hcmut.edu.vn/tts/speak", {
+        text: word,
+        gender: "male",
+      })
+      .then((res) => {
+        let base64sound;
+        let audio;
+        base64sound = res.data.speech;
+        console.log(base64sound);
+        audio = new Audio(`data:audio/wav;base64,${res.data.speech}`);
+        audio.play();
+      });
+  };
+
   return (
     <div className="card">
       <div className="flex-vertical content">
@@ -13,21 +30,23 @@ function Card({ word, pos, BinhDinh, GiaLai, KonTum }) {
       </div>
       <div className="flex-horizontal">
         <div className="sound">
-            <button disabled={BinhDinh === "-"}>
-              <AiFillSound />
-              <span>BinhDinh</span>
-            </button>
-            <button disabled={GiaLai === "-"}>
-              <AiFillSound />
-              <span>Gia Lai</span>
-            </button>
-            <button disabled={KonTum === "-"}>
-              <AiFillSound />
-              <span>KonTum</span>
-            </button>
+          <button disabled={BinhDinh === "-"} onClick={(e) => playSound(e)}>
+            <AiFillSound />
+            <span>BinhDinh</span>
+          </button>
+          <button disabled={GiaLai === "-"} onClick={(e) => playSound(e)}>
+            <AiFillSound />
+            <span>Gia Lai</span>
+          </button>
+          <button disabled={KonTum === "-"} onClick={(e) => playSound(e)}>
+            <AiFillSound />
+            <span>KonTum</span>
+          </button>
         </div>
-        <IconContext.Provider value={{size: "45px"}}>
-        <div className="bookmark"><FaRegBookmark/></div>
+        <IconContext.Provider value={{ size: "45px" }}>
+          <div className="bookmark">
+            <FaRegBookmark />
+          </div>
         </IconContext.Provider>
       </div>
     </div>
