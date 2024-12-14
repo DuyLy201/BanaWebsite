677af9d5f884ac2aa6b6417bebd9a9216f508f71
DuyLy <1652099@hcmut.edu.vn>
2024-12-14 20:54:20 +0700

    Handle play sound with mp3 file in translation page

--- src/Pages/Translation.js
+++ src/Pages/Translation.js
@@ -6,9 +6,12 @@ import myImage from "../Assets/Images/Bilingual.jpg";
 
 function Translation() {
   const languages = ['BinhDinh', 'GiaLai', 'KonTum'];
+  const genders = ['male', 'female'];
   const [searchValue, setSearchValue] = useState('');
   const [language, setLanguage] = useState(languages[0]);
+  const [gender, setGender] = useState(genders[0]);
   const [data, setData] = useState('');
+  const [audioSrc, setAudioSrc] = useState('');
 
   useEffect(() => {
     setSearchValue("");
@@ -20,10 +23,26 @@ function Translation() {
 
   const handleButtonClick = () => {
     axios
-      .post(`http://faws.gvlab.org/fablab/ura/llama/bahnar/api/translate/vi_ba`, { region: language, text: searchValue }) // update api translation here
+      .post(`https://www.ura.hcmut.edu.vn/bahnar/nmt/api/translateBahnar`, { region: language, text: searchValue }) // update api translation here
       .then((response) => {
-        console.log(response.data.tgt);
-        setData(response.data.tgt);
+        // console.log("duylh",response.data.payload);
+        setData(response.data.payload.tgt);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const handleVoiceButtonClick = () => {
+    axios
+      .post(`https://www.ura.hcmut.edu.vn/bahnar/nmt/api/translateBahnar/voice`, { gender: gender, text: searchValue }, { responseType: 'blob' }) // update api translation here
+      .then((response) => {
+        const url = URL.createObjectURL(response.data);
+        setAudioSrc(url);
+        const audio = new Audio(url);
+        audio.play().catch((error) => {
+          console.log("Error playing audio:", error);
+        });
       })
       .catch((error) => {
         console.log(error);
@@ -47,6 +66,9 @@ function Translation() {
         <svg width="13" height="20" viewBox="0 0 13 20" fill="#1C45F9" xmlns="http://www.w3.org/2000/svg" className="ml-[-8px] h-4 w-4">
           <path d="M0.879883 2.12L8.75988 10L0.879883 17.88L2.99988 20L12.9999 10L2.99988 0L0.879883 2.12Z" fill="current" fill-opacity="0.87"></path>
         </svg>
+        <svg width="13" height="20" viewBox="0 0 13 20" fill="#1C45F9" xmlns="http://www.w3.org/2000/svg" className="ml-[-8px] h-4 w-4">
+          <path d="M0.879883 2.12L8.75988 10L0.879883 17.88L2.99988 20L12.9999 10L2.99988 0L0.879883 2.12Z" fill="current" fill-opacity="0.87"></path>
+        </svg>
         <p style={{marginLeft:'20px'}}>Bahna</p>
       </div>
       <div className="translation-container">
@@ -59,18 +81,23 @@ function Translation() {
           style={{width: '100%', border: 'none'}}
         />
         <div className="translation-right">
-          Hiển thị kết quả ở đây
           {data ?? ""}
         </div>
       </div>
       <div style={{display:'flex', gap:'30px'}}>
-        <button style={{flex:1}} className='button-container' onClick={switchLanguage}>
-          {language}
-        </button>
         <button style={{flex:1}} className='button-container' onClick={handleButtonClick}>
           Dịch
         </button>
+        <button style={{flex:1}} className='button-container' onClick={handleVoiceButtonClick}>
+          Dịch bằng âm thanh
+        </button>
       </div>
+      {audioSrc && (
+        <audio controls autoPlay>
+          <source src={audioSrc} type="audio/mpeg" />
+          Your browser does not support the audio element.
+        </audio>
+      )}
     </>
   );
 }
